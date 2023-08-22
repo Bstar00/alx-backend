bb90ea968a03d85851d607c8fb1653bd97b84875
Bstar00 <samueloriola212@gmail.com>
2023-08-22 17:15:56 +0000

    task 1

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,25 +1,31 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """
-Basic dictionary script
+Basic caching script
 """
 from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
     """
-    The caching system class
+    Basic caching system that inherits from BaseCaching.
     """
+
+    def __init__(self):
+        """
+        Constructor for initializing the cache.
+        """
+        super().__init__()
 
     def put(self, key, item):
         """
-        Add an item to the cache
+        Add an item to the cache.
         """
         if key is not None and item is not None:
             self.cache_data[key] = item
 
     def get(self, key):
         """
-        Retrieve the value of a cache item
+        Retrieve the value associated with a key from the cache.
         """
         if key is None or key not in self.cache_data:
             return None
